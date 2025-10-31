fd2e49a9ab003364f37180cf82693a28a52f77df
Roman <roman.anokhin.work@gmail.com>
2025-10-31 19:21:57 +0200

    initial commit

---
+++ src/components/App-component/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo-component/CafeInfo'
+
+function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo/>
+   </div>
+  )
+}
+
+export default App
